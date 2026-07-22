03ff81ec4b958cbf78d44059809f6e08eb44b3ff
정재현 <chwogus0303@naver.com>
2026-07-22 10:42:42 +0900

    init: initialize Spring Boot backend

---
+++ backend/src/test/java/org/ssafy/b102/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ssafy.b102.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
