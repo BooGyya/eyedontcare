7d36614e8d734520194f3e75f8d6fec9c494445a
Taieun Kim <twilight1619@gmail.com>
2026-08-06 00:03:06 +0900

    fix(frontend): 준비방 카메라 영상 잘림 수정

--- frontend/src/pages/GameReadyPage.vue
+++ frontend/src/pages/GameReadyPage.vue
@@ -2436,7 +2436,7 @@ onBeforeUnmount(() => {
   inset: 0;
   width: 100%;
   height: 100%;
-  object-fit: cover;
+  object-fit: contain;
   transform: scaleX(-1);
 }
 .participant-visual__camera--peer {
